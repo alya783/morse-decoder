c2b25799a66934679b0c828635881fae9484c595
alya783 <76521831+alya783@users.noreply.github.com>
2021-08-23 22:42:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let words = [];
+    let wordSize = 10;
+    while(expr.length > 0){
+         let oneWord = expr.slice(0, wordSize);
+         words.push(oneWord);
+         expr = expr.slice(wordSize);
+    }
+
+     let morse = [];
+     for(let i = 0; i < words.length; i++){ 
+        let numb = [];
+        let numbSize = 2;
+        while(words[i].length> 0){
+            let number = words[i].slice(0, numbSize);
+            if (number !== '00'){
+				numb.push(number);
+			}
+            words[i] = words[i].slice(numbSize);
+		}
+        let decode = [];
+		 for(let j = 0; j < numb.length; j++){
+            if(numb[j] === '11'){ 
+               decode.push("-");
+            } else if(numb[j] === '10'){
+               decode.push('.');
+            }
+        }
+		 
+		morse.push(decode.join(''));  	
+    }  
+    console.log(morse);
+
+    let arr = [];
+    for(let p = 0; p < morse.length; p++){
+       arr.push(MORSE_TABLE[morse[p]]);
+    }
+
+    return arr.join('');
 }
 
 module.exports = {
